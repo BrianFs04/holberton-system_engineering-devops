4722ffe304f32922cd71cbfdc9d78aee05dfb66c
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-03-25 22:10:33 +0000

    [Fix]: Typo

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,5 @@
 # Install Nginx web-server using puppet
 exec { 'installNginx':
-  $part1 = "sudo apt-get -y install nginx ; echo \"Holberton School\" | sudo tee /var/www/html/index.nginx-debian.html",
-  $mycommand = "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/",
-  command  =>  'sudo apt-get -y update ; $part1 ;  sudo sed -i "$mycommand" /etc/nginx/sites-avaliable/default ; sudo service nginx start',
+  command  =>  'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo \"Holberton School\" | sudo tee /var/www/html/index.nginx-debian.html ;  sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
   provider =>  shell,
 }
