5d77328b8ceedda7c02031111cd2dafea0882076
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-03-25 05:09:17 +0000

    [Update]: Adding tee

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # Install Nginx web-server using puppet
 exec { 'installNginx':
-  command  =>  'sudo apt-get -y update ;sudo apt-get -y install nginx ; echo "Holberton School" > /var/www/html/index.nginx-debian.html ; sed -i "s/server_name _;/\n\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-avaliable/default ; sudo service nginx start',
+  command  =>  'sudo apt-get -y update ;sudo apt-get -y install nginx ; echo "Holberton School" > sudo tee /var/www/html/index.nginx-debian.html ; sed -i "s/server_name _;/\n\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-avaliable/default ; sudo service nginx start',
   provider =>  shell
 }
