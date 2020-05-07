a2cc5113d02786198bd4e89ff07b671fe9a294d7
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-05-07 23:37:14 +0000

    [Fix]: Return value

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,19 +4,18 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after)
     headers = {
         'User-Agent': '1.0'
     }
     params = {
-        'limit': 100,
         'after': after
     }
     response = requests.get(url, allow_redirects=False, headers=headers,
                             params=params)
-    r = response.json()
 
-    if response:
+    if response.status_code is 200:
+        r = response.json()
         x = r.get('data').get('children')
         aft_val = r.get('data').get('after')
         a = list(map(lambda i: hot_list.append(i.get('data').get('title')), x))
@@ -24,4 +23,4 @@ def recurse(subreddit, hot_list=[], after=None):
             return (hot_list)
         return (recurse(subreddit, hot_list, aft_val))
     else:
-        print(None)
+        return(None)
