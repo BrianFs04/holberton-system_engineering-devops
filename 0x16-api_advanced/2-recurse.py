12ae4476c2a9f34bbb6a4dbbe16580dc32a601e1
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-05-07 23:44:15 +0000

    [Fix]: Typo

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,17 +4,18 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
         'User-Agent': '1.0'
     }
     params = {
+        'limit': 100,
         'after': after
     }
     response = requests.get(url, allow_redirects=False, headers=headers,
                             params=params)
 
-    if response.status_code is 200:
+    if response.status_code == 200:
         r = response.json()
         x = r.get('data').get('children')
         aft_val = r.get('data').get('after')
