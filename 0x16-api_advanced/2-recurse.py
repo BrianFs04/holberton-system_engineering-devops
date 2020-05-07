7b447d506de4652d2a9aa50467b11e98dc89c769
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-05-07 23:25:49 +0000

    [Feature]: Recursive pagination

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Pagination using recursion"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        'User-Agent': '1.0'
+    }
+    params = {
+        'limit': 100,
+        'after': after
+    }
+    response = requests.get(url, allow_redirects=False, headers=headers,
+                            params=params)
+    r = response.json()
+
+    if response:
+        x = r.get('data').get('children')
+        aft_val = r.get('data').get('after')
+        a = list(map(lambda i: hot_list.append(i.get('data').get('title')), x))
+        if not aft_val:
+            return (hot_list)
+        return (recurse(subreddit, hot_list, aft_val))
+    else:
+        print(None)
