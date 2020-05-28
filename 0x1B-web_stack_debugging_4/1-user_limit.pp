56d5916b61f4d39e9c1a546c40fa9ee834c1c939
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-05-28 17:03:06 +0000

    [Fix]: Puppet-lint style by adding variables

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,7 @@
 # OS configuration for holberton user
 exec { 'change-os-configuration-for-holberton-user':
-  command  => 'sudo sed -i "s/hard nofile 5/hard nofile 40000/g" /etc/security/limits.conf;sudo sed -i "s/soft nofile 4/soft nofile 20000/g" /etc/security/limits.conf; sudo sysctl -p',
-  provider => 'shell',
+  environment => ['FILE=/etc/security/limits.conf',
+      'HARD=hard nofile', 'SOFT=soft nofile'],
+  command     => 'sudo sed -i "s/$HARD 5/$HARD 40000/g" $FILE;sudo sed -i "s/$SOFT 4/$SOFT 20000/g" $FILE; sudo sysctl -p',
+  provider    => 'shell',
 }
